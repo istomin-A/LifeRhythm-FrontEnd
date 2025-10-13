3a25fbb9a78657954b1e58255f093f48f243d9bb
Andrey <ivankolecnik19@gmail.com>
2025-10-14 00:46:32 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/LifeRhythm-FrontEnd/dist/',
+  base: '/LifeRhythm-FrontEnd/',
   plugins: [
     react(),
     svgr(),
