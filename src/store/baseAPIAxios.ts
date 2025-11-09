6330879db608d78c55bb2a58074bc28c4488811c
Andrey <ivankolecnik19@gmail.com>
2025-11-09 21:16:28 +0200

    fix

--- src/store/baseAPIAxios.ts
+++ src/store/baseAPIAxios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_API_DEV_URL,
+  baseURL: import.meta.env.VITE_BACKEND_API_PROD_URL,
   headers: {
     "Content-Type": "application/json",
   },
